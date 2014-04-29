30747e2099d0e7104f9790517341ba27e139c72b
LM10 <luis.matute@me.com>
2014-04-29 12:56:03 -0600

    - Add Watch, Uglify and Imagemin to grunt - Add index.html for no reason - Add random assets

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,26 +5,103 @@ var grunt_wrap = function (grunt) {
 		pkg: grunt.file.readJSON('package.json'),
 		sass: {
 			dist: {
+				expand: true,
+				cwd: 'assets/sass/',
+				src: ['**/*.scss', '!**/_*.scss'],
+				dest: 'dist/css',
+				ext: '.css',
 				options: {
-					banner: '/*! <%= pkg.name %> - Last Update <%= grunt.template.today("mmmyy") %> */',
-					style: 'compressed'
+					style: "compressed"
+				}
+			}
+		},
+		uglify: {
+			build: {
+				options: {
+					report: 'gzip',
+					mangle: {
+						except: ['!*.min.js']
+					}
 				},
 				files: [{
 					expand: true,
-					cwd: 'assets/sass',
-					src: ['**.*scss'],
-					dest: 'assets/css',
-					ext: '.css'
+					cwd: 'assets/js',
+					src: '**/*.js',
+					dest: 'dist/js/',
+					ext: '.min.js'
 				}]
 			}
 		},
-		jsmin: {},
-		imagemin: {},
-		watch: {}
+		imagemin: {
+			png: {
+				options: {
+					optimizationLevel: 3
+				},
+				files: [
+					{
+						expand: true,
+						cwd: 'assets/img/',
+						src: ['**/*.png', '!compressed/**/*.png'],
+						dest: 'dist/img/compressed/',
+						ext: '.png'
+					}
+				]
+			},
+			jpg: {
+				options: {
+					progressive: true
+				},
+				files: [
+					{
+						expand: true,
+						cwd: 'assets/img/',
+						src: ['**/*.jpg','**/*.jpeg', '!compressed/**/*.{jpg,jpeg}'],
+						dest: 'dist/img/compressed/',
+						ext: '.jpg'
+					}
+				]
+			},
+			gif: {
+				options: {
+					interlaced: true
+				},
+				files: [{
+					expand: true,
+					cwd: 'assets/img/',
+					src: ['**/*.gif', '!compressed/**/*.gif'],
+					dest: 'dist/img/compressed/',
+					ext: '.gif'
+				}]
+			}
+		},
+		watch: {
+			css: {
+				files: 'assets/sass/**/*.scss',
+				tasks: ['sass'],
+				options: {
+					spawn: false,
+				}
+			},
+			scripts: {
+				files: ['assets/js/**/*.js'],
+				tasks: ['uglify'],
+				options: {
+					spawn: false,
+				}
+			},
+			images: {
+				files: ['assets/img/**/*.{png,jpg,jpeg,gif}', 'assets/img/*.{png,jpg,jpeg,gif}'],
+				tasks: ['imagemin'],
+				options: {
+					spawn: false,
+				}
+			}
+		}
 	})
 
 	// Load Dependent Plugins ==================/
 	grunt.loadNpmTasks('grunt-contrib-sass')
+	grunt.loadNpmTasks('grunt-contrib-uglify')
 	grunt.loadNpmTasks('grunt-contrib-imagemin')
 	grunt.loadNpmTasks('grunt-contrib-watch')
 
@@ -33,17 +110,3 @@ var grunt_wrap = function (grunt) {
 }
 
 module.exports = grunt_wrap
-
-
-
-
-/*
-	/assets
-	  ∟ css/
-	  ∟ js/
-	  ∟ img/
-	  ∟ sass/
-	Gruntfile.js
-	index.html
-	package.json
-*/
---
+++ assets/js/app.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+	var randomasignment = 0;
+
+	alert(1);
+	var test = 'wow this looks awesome';
+});
